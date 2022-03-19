496438f33c101ff7fc3180f622d3fa4a15d92d2f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-19 00:35:58 -0300

    Criando componentes e construindo o projeto

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: 'course-list.component.html'
+})
+export class CourseListComponent implements OnInit {
+
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPTS-1151',
+                duration: 120,
+                rating: 4.5,
+                releaseDate: 'December, 2, 2021'
+            },
+            {
+                id: 2,
+                name: 'Typescript: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'TPTS-2262',
+                duration: 80,
+                rating: 5.0,
+                releaseDate: 'January, 1, 2022'
+            }
+        ]
+    }
+
+    
+
+}
---
+++ src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course {
+    id: number = 0;
+    name: string = '';
+    imageUrl: string = '';
+    price: number = 0;
+    code: string = '';
+    duration: number = 0;
+    rating: number = 0;
+    releaseDate: String = '';
+}
