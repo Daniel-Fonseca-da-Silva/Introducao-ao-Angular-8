41bae5489cda21f77e047b6a4abf9369e3160357
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-19 23:33:59 -0300

    Protegendo Rotas com Guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './stars/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NotFound404Component } from './not-found-404/not-found-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    NotFound404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      // Rota padrão ou root ''
+      {
+        path: '', redirectTo: 'Courses', pathMatch: 'full'
+      },
+      // Rota para o recurso
+      {
+        path: 'Courses', component: CourseListComponent
+      },
+      {
+        // Rota ** quando outra rota não funcionar
+        path: '**', component: NotFound404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/not-found-404/not-found-404.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './not-found-404.component.html'
+})
+export class NotFound404Component{
+
+}
