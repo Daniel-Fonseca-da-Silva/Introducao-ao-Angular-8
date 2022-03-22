fad6189fe37bfee959d9b45e33ad822c08962dd7
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-22 00:12:26 -0300

    Trabalhando com delete

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -32,6 +32,16 @@ export class CourseListComponent implements OnInit {
         });
     }
 
+    deleteById(courseId: number): void {
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => {
+                console.log('Deleted with success');
+                this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        });
+    }
+
     set filter(value: string) {
         this._filterBy = value;
         this.filteredCourses = this._courses.filter((course) =>
