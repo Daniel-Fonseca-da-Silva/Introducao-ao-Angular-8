97138e972923fdd2b807642db7622281aea360d6
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-24 22:11:52 -0300

    Conhecendo a pasta shared

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { ReplacePipe } from "../pipe/replace.pipe";
-import { StarComponent } from "../stars/star.component";
+import { AppPipeModule } from "../pipe/pipe/app.pipe.module";
+import { StarModule } from "../shared/component/stars/star.module";
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
 
@@ -11,12 +11,12 @@ import { CourseListComponent } from "./course-list.component";
     declarations: [
         CourseListComponent,
         CourseInfoComponent,
-        ReplacePipe,
-        StarComponent
     ],
     imports: [
         FormsModule,
         CommonModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             // Rota para o recurso
         {
---
+++ src/app/shared/component/stars/star.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { StarComponent } from "./star.component";
+
+@NgModule({
+    declarations: [
+        StarComponent
+    ],
+    exports: [
+        StarComponent
+    ]
+})
+export class StarModule{}
